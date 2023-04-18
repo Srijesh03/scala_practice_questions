dfa648af88a2238cd37790b940f894085b7cd255
srijesh <mailtosrijesh@gmail.com>
2023-04-18 17:55:24 +0530

    pushed in another branch just to check

---
+++ src/main/scala/q4.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.sql.SparkSession
+
+import scala.io.StdIn
+
+object q4 extends App {
+
+  val spark = SparkSession.builder()
+    .appName("question4")
+    .master("local[*]")
+    .getOrCreate()
+
+  //1store a number in vairable
+  println("Please enter a number")
+  val num: Int = StdIn.readInt()
+
+  //2if value is not within range of (100-1000) print number is not within the range
+  if (num >= 100 && num <= 1000) {
+    println("Number is in range")
+  } else {
+    println("number is out of range")
+  }
+
+  //check for even or odd
+
+  if (num%2 == 0)
+    {println("Number is even")}
+  else
+    {println("Number is odd")}
+
+
+}
+
+
+
+
+
